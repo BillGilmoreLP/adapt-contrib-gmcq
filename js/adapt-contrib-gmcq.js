f53a35d04d70938fae966100fe8e5108b8852f0f
Guy Willis <guy.willis@kineo.com>
2018-07-27 14:20:45 +0100

    Class name refactor

--- js/adapt-contrib-gmcq.js
+++ js/adapt-contrib-gmcq.js
@@ -6,15 +6,15 @@ define([
   var Gmcq = Mcq.view.extend({
 
     events: {
-      'focus .gmcq-item input': 'onItemFocus',
-      'blur .gmcq-item input': 'onItemBlur',
-      'change .gmcq-item input': 'onItemSelected',
-      'keyup .gmcq-item input': 'onKeyPress'
+      'focus .js-mcq-item-input': 'onItemFocus',
+      'blur .js-mcq-item-input': 'onItemBlur',
+      'change .js-mcq-item-input': 'onItemSelected',
+      'keyup .js-mcq-item-input': 'onKeyPress'
     },
 
     onItemSelected: function(event) {
 
-      var selectedItemObject = this.model.get('_items')[$(event.currentTarget).parent('.gmcq-item').index()];
+      var selectedItemObject = this.model.get('_items')[$(event.currentTarget).parent('.js-mcq-item').index()];
 
       if (this.model.get('_isEnabled') && !this.model.get('_isSubmitted')) {
         this.toggleItemSelected(selectedItemObject, event);
@@ -37,7 +37,7 @@ define([
       this.resizeImage(Adapt.device.screenSize);
       this.setUpColumns();
 
-      this.$('label').imageready(_.bind(function() {
+      this.$('.js-mcq-item-label').imageready(_.bind(function() {
         this.setReadyStatus();
       }, this));
 
@@ -50,7 +50,7 @@ define([
     resizeImage: function(width) {
       var imageWidth = width === 'medium' ? 'small' : width;
 
-      this.$('label').each(function(index) {
+      this.$('.js-mcq-item-label').each(function(index) {
         var $img = $(this).find('img');
         var newSrc = $img.attr('data-' + imageWidth);
         if (newSrc) {
@@ -62,15 +62,14 @@ define([
 
     setUpColumns: function() {
       var columns = this.model.get('_columns');
-
       if (!columns) return;
 
+      this.$el.addClass('gmcq-column-layout');
+
       if (Adapt.device.screenSize === 'large') {
-        this.$el.addClass('gmcq-column-layout');
-        this.$('.gmcq-item').css('width', (100 / columns) + '%');
+        this.$('.js-mcq-item').css('width', (100 / columns) + '%');
       } else {
-        this.$el.removeClass('gmcq-column-layout');
-        this.$('.gmcq-item').css('width', '');
+        this.$('.js-mcq-item').css('width', '');
       }
     }
 
